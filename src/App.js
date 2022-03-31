3238d53ba0eb0609167285d50e524c712609a8a6
shorif4321 <shorifali4321@gmail.com>
2022-04-01 03:13:39 +0600

    ali

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          start react for oodlecarfinance
         </a>
       </header>
     </div>
